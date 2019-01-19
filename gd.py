4b84c65b4c9d22d3c7617a40f95b5b3a04665f96
foobar98 <pranavgupta998@gmail.com>
2019-01-19 14:47:38 +0530

    gradient descent

--- gd.py
+++ gd.py
@@ -5,49 +5,65 @@ fig = plt.figure()
 
 x = np.array([])
 y = np.array([])
-m = 0
-c = 0
+rate = 0.0005       # learning rate (alpha)
+max_iter = 10000
 
-def linear_regression_gd():    
-    global m,c
-    # Line parameters
-    y_gen = np.array([m*i+c for i in x])
-    #print((y-y_gen).shape)
-
-    error = y-y_gen
-    print(error)
-    deltaM = sum(x*error)
-    deltaC = sum(error)
-
-    #print(deltaM,deltaC)
-
-    m = m+deltaM
-    c = c+deltaC
-
-    #print(m,c)
+def draw(theta):
+    y_guess = np.array([theta[0]+theta[1]*i for i in x])
 
     plt.clf()
-    plt.axis([0,100,0,100])  
+    plt.axis([-20,20,-20,20])  
 
     # Plotting data points
     plt.scatter(x,y,color='black')
 
     # Plotting hypothesis line
-    plt.plot(x,y_gen,'b')
+    plt.plot(x,y_guess,'b')
+
+    # Uncomment below line for simulation
+    # plt.pause(0.01)
     plt.draw()
 
+def gradientDescent():
+    x_trans = x.transpose()
+
+    # Initialize theta
+    theta = np.zeros(2)
+    m = x.size
+
+    for i in range(0,max_iter):
+        hypothesis = np.array([theta[0]+theta[1]*i for i in x])
+        error = hypothesis-y
+
+        # Cost function
+        J = np.sum(error**2)/(2*m)
+
+        #print("Iteration %d | Cost: %f" %(i,J))
+
+        # Calculate gradient
+        del0 = np.sum(error)/m
+        del1 = np.sum(x_trans*error)/m
+
+        theta[0] = theta[0]-rate*del0
+        theta[1] = theta[1]-rate*del1
+
+        # Uncomment below line for simulation
+        # draw(theta)
+        
+    draw(theta)
+
 def onclick(event):
-    global ix,iy,x,y
+    global x,y
     ix,iy = event.xdata,event.ydata
-    #print('x = %d, y = %d'%(ix, iy))
+    print('x = %d, y = %d'%(ix, iy))
 
     # Add new mouse click coordinates
     x = np.append(x,ix)
     y = np.append(y,iy)
 
-    linear_regression_gd()
+    gradientDescent()
 
 fig.canvas.mpl_connect('button_press_event', onclick)
 
-plt.axis([0,100,0,100])  
+plt.axis([-20,20,-20,20])  
 plt.show()
